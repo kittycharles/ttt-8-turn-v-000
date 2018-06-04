59686b2f862b7016437ae8ca24f55ea9726fa040
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-04 15:36:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,16 +13,9 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  # binding.pry
   !position_taken?(board, index) && index.between?(0, 8) ? true : false
 end
 
 def position_taken?(board, index)
    board[index] == " " || board[index] == "" || board[index] == nil ? false : true
-
-  # if board[index] == " " || board[index] == "" || board[index] == nil
-  #   false
-  # elsif board[index] == "X" || board[index] == "O"
-  #   true
-  # end
 end
