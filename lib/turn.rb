9423e08aa991dec3078bbccb618c3e7f38051c5a
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-04 17:22:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,5 +29,7 @@ def turn(board)
     user_input = gets.strip
     index = input_to_index(user_input)
     valid_move?(board, index)
-    turn(board)
+    if valid_move(board, index) == false
+      turn(board)
+    end
 end
