875b70ade7110b8192f2a77eaa27364c9100d20c
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-04 14:47:02 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+display_board
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if board[input_to_index] == " "
+    true
+  elsif position_taken? == true
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
